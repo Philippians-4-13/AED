33d9d7b6ae7609c23fb075202b4cf6326d41a050
Pablo Park <66440894+Philippians-4-13@users.noreply.github.com>
2021-04-12 13:48:58 -0300

    Add files via upload

---
+++ 00-CppHelloWorld/src/00-CppHelloWorld.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : 00-CppHelloWorld.cpp
+// Author      : Pablo Park
+// Version     : Eclipse 2.4
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout<<"Hello World!"<<endl; // prints Hello World!
+   return 0;
+}
