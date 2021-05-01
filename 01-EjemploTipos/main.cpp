4642ad3869dc220534ca301397f0d8034099acf9
Pablo Park <66440894+Philippians-4-13@users.noreply.github.com>
2021-05-01 18:06:43 -0300

    Macro

---
+++ 01-EjemploTipos/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cassert>
+#define NDBUG
+
+using namespace std;
+
+int main() 
+{
+    bool vTrue = true, vFalse = false;
+    assert (vTrue&&vFalse);
+    assert (vTrue||vFalse);
+    assert (!vFalse);
+
+    vTrue = 1;
+    vFalse = 0;
+
+    assert (vTrue + vFalse); 
+    assert (vTrue - vFalse);
+    assert (vTrue = 1 > 2);
+    
+    unsigned uA = 6, uB = 3; 
+	assert(uA + uB);
+    assert(uA - uB);
+    assert(uA * uB);
+    assert(uA / uB);
+    assert(uA % uB);
+
+    int iA = -2, iB = 12;
+	assert(iA + iB);
+    assert(iA - iB);
+    assert(iA * iB);
+    assert(iA / iB);
+
+    double dA = 2.4, dB = 3.5;
+    assert(dA + dB);
+    assert(dA - dB);
+    assert(dA * dB);
+    assert(dA / dB);
+
+    char cA ='A', cB ='B';
+    assert(cA>cB);
+    assert(cA+cB+cB+cA);
+
+    string sA = "Abecedario", sB ="012345";
+    string sC = sA + sB;
+}
---
+++ 01-EjemploTipos/readme.md
@@ -0,0 +1,11 @@
+- __Etapa #1: Análisis del Problema.__
+◦ Transcripción del problema.
+◦ Refinamiento del problema e hipótesis de trabajo.
+◦ Modelo IPO con:
+▪ Entradas: nombres y tipos de datos.
+▪ Proceso: nombre descriptivo.
+▪ Salidas: nombres y tipos de datos.
+- __Etapa #2: Diseño de la solución.__ Consta del algoritmo que define el método
+por el cual el proceso obtiene las salidas a partir de las entradas:
+◦ Léxico del Algoritmo.
+◦ Representación visual ó textual del Algoritmo
