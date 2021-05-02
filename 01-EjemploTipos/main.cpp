d7a98ad61d5a9e48860dd4b8890274c84d361b2b
Pablo Park <66440894+Philippians-4-13@users.noreply.github.com>
2021-05-02 18:23:13 -0300

    Update main.cpp

--- 01-EjemploTipos/main.cpp
+++ 01-EjemploTipos/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() 
 {
     bool vTrue = true, vFalse = false;
-    assert (vTrue&&vFalse);
+    assert (vTrue&&vFalse||vTrue);
     assert (vTrue||vFalse);
     assert (!vFalse);
 
@@ -15,16 +15,16 @@ int main()
 
     assert (vTrue + vFalse); 
     assert (vTrue - vFalse);
-    assert (vTrue = 1 > 2);
+    assert (vTrue = 1 < 2);
     
-    unsigned uA = 6, uB = 3; 
+    unsigned uA = 7, uB = 3; 
 	assert(uA + uB);
     assert(uA - uB);
     assert(uA * uB);
     assert(uA / uB);
     assert(uA % uB);
 
-    int iA = -2, iB = 12;
+    int iA = -24, iB = 12;
 	assert(iA + iB);
     assert(iA - iB);
     assert(iA * iB);
@@ -37,7 +37,7 @@ int main()
     assert(dA / dB);
 
     char cA ='A', cB ='B';
-    assert(cA>cB);
+    assert(cA<cB);
     assert(cA+cB+cB+cA);
 
     string sA = "Abecedario", sB ="012345";
